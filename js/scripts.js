0ea3cdd61ffcbc8b8289b2a72079fd85e1d069f4
Rich Garrick <richg341@gmail.com>
2017-07-13 10:05:38 -0700

    add topping selectors and refine size selectors

--- js/scripts.js
+++ js/scripts.js
@@ -11,25 +11,25 @@
 //   return this.pizzaCost;
 // }
 //
-// window.onload = function() {
-//   document.getElementById("LearnMoreBtn").onclick = function(){
-//         var overlay = document.getElementById("overlay");
-//         var popup = document.getElementById("popup");
-//         overlay.style.display = "block";
-//         popup.style.display = "block";
-//     };
-//
-//   document.getElementById("CloseBtn").onclick = function(){
-//         var overlay = document.getElementById("overlay");
-//         var popup = document.getElementById("popup");
-//         overlay.style.display = "none";
-//         popup.style.display = "none";
-//   }
-// };
+window.onload = function() {
+  document.getElementById("LearnMoreBtn").onclick = function(){
+        var overlay = document.getElementById("overlay");
+        var popup = document.getElementById("popup");
+        overlay.style.display = "block";
+        popup.style.display = "block";
+    };
+
+  document.getElementById("CloseBtn").onclick = function(){
+        var overlay = document.getElementById("overlay");
+        var popup = document.getElementById("popup");
+        overlay.style.display = "none";
+        popup.style.display = "none";
+  }
+};
 
 $(document).ready(function() {
   $("form#pizza").submit(function(event) {
-    
+console.log("test");
 
 
     $("#receipt").show();
